f157e858016a6cdc44989d8db8069993d57f5244
Peter Fletcher <peterfletcher@peters-mbp-2.home>
2023-01-29 15:13:11 +0000

    feat(neovim) - add neovim to macOS

---
+++ lua/pete/core/keymaps.lua
@@ -0,0 +1,52 @@
+local opts = { noremap = true, silent = true }
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- general
+keymap.set("n", "<leader>x", ":<CR>", opts)
+
+-- insert
+keymap.set("i", "jk", "<ESC>")
+
+
+-- normal
+keymap.set("n", "<leader>pv", vim.cmd.Ex)
+keymap.set("n", "<leader>l", "$")
+keymap.set("n", "<leader>h", "_")
+
+-- delete single character without copying into register
+keymap.set("n", "x", '"_x')
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>") -- increment
+keymap.set("n", "<leader>-", "<C-x>") -- decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+
+-- surround
+-- to surround word with <any> - ysw<char>
+-- to delete surround word with <any> - dsw<char>
+-- to change surround word with <any> - csw<char>
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
---
+++ lua/pete/core/options.lua
@@ -0,0 +1,47 @@
+local opt = vim.opt
+
+opt.scrolloff = 8
+opt.updatetime = 50
+opt.colorcolumn = "100"
+
+-- line numbers 
+opt.relativenumber = true
+opt.number = true
+
+-- tabbing
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
+
+-- appearance
+opt.cursorline = true
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append('unnamedplus')
+
+-- windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append('-') -- considers dash as word
+
+opt.swapfile = false
+opt.backup = false
+-- opt.undodir = os.geten("HOME") .. "/.vim/undodir"
+opt.undofile = true
